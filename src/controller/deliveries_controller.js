b9e146606a5c6bcdaa533547d59df966bd850237
Mikael David <63308965+MikaelDDavidd@users.noreply.github.com>
2024-12-06 22:52:59 -0300

    Update deliveries_controller.js

--- src/controller/deliveries_controller.js
+++ src/controller/deliveries_controller.js
@@ -27,7 +27,8 @@ export async function postNewDelivery(req, res) {
 }
 
 // Função para upload de imagem na VPS
-exports.uploadProductImage = async (req, res, next) => {
+// Função para upload de imagem na VPS
+export const uploadProductImage = async (req, res, next) => {
   const file = req.file;
 
   try {
